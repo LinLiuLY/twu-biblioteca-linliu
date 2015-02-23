488572c6359cce3d1bedd3838078be5da8b61207
Lin Liu <linliu@thoughtworks.com>
2015-02-23 18:09:41 +0800

    [LL] - use properties to store movie information

--- src/com/twu/biblioteca/constants/ConstantProductProperty.java
+++ src/com/twu/biblioteca/constants/ConstantProductProperty.java
@@ -6,13 +6,14 @@ public class ConstantProductProperty {
     public static final String AUTHOR = "author";
     public static final String YEAR = "yearPublished";
     public static final String STATUS = "status";
+    public static final String RATING = "rating";
 
     public static final String JAVA = "HeadFirstJava";
     public static final String JAVASCRIPT = "HeadFirstJavaScript";
     public static final String HTML = "HeadFirstHtml";
 
-    public static final String MOVIE_ONE = "one";
-    public static final String MOVIE_TWO = "two";
+    public static final String MOVIE_ONE = "movieOne";
+    public static final String MOVIE_TWO = "movieTwo";
 
     public static final String BOOK_TYPE = "book";
     public static final String MOVIE_TYPE = "movie";
--- test/com/twu/biblioteca/product/ProductInfoTest.java
+++ test/com/twu/biblioteca/product/ProductInfoTest.java
@@ -7,19 +7,24 @@ import static org.junit.Assert.assertTrue;
 
 public class ProductInfoTest {
     @Test
-    public void isBookCheckedOutTest() {
+    public void isProductCheckedOutTest() {
         assertFalse("book is not checked out", new ProductInfo().isProductCheckedOut("HeadFirstJava"));
+        assertFalse("movie is not checked out", new ProductInfo().isProductCheckedOut("movieOne"));
     }
 
     @Test
-    public void isBookBelongsToLibraryTest() {
+    public void isProductBelongsToLibraryTest() {
         assertTrue("book belongs to the library", new ProductInfo().isProductBelongsToLibrary("book", "HeadFirstJavaScript"));
         assertFalse("book does not belong to the library", new ProductInfo().isProductBelongsToLibrary("book", "CSS"));
+        assertTrue("movie belongs to the library", new ProductInfo().isProductBelongsToLibrary("movie", "movieOne"));
+        assertFalse("movie does not belong to the library", new ProductInfo().isProductBelongsToLibrary("movie", "movieThree"));
     }
 
     @Test
-    public void isBookAvailableToCheckout() {
+    public void isProductAvailableToCheckout() {
         assertTrue("book is available", new ProductInfo().isProductAvailableToCheckout("book", "HeadFirstHtml"));
         assertFalse("book is unavailable", new ProductInfo().isProductAvailableToCheckout("book", "CSS"));
+        assertTrue("movie is available", new ProductInfo().isProductAvailableToCheckout("movie", "movieTwo"));
+        assertFalse("movie is unavailable", new ProductInfo().isProductAvailableToCheckout("movie", "movieThree"));
     }
 }
